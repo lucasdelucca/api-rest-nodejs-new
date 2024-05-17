9fb42aa205c280e56419b7a12e87f39aa3b953a4
Lucas de Lucca <llucca54@gmail.com>
2024-05-17 11:50:44 -0300

    chore: change node engine to version 18.18.0

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import { app } from './app'
 import { env } from './env'
 
 const port = env.PORT
-app.listen({ port }).then(() => {
-  console.log('🚀 Started server at http://localhost:' + port)
-})
+app
+  .listen({
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    port,
+  })
+  .then(() => {
+    console.log('🚀 Started server at http://localhost:' + port)
+  })
