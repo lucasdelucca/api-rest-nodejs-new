12503f86a2ffbd56f90466c435acd3b208cc2bc7
Lucas de Lucca <llucca54@gmail.com>
2024-05-16 22:23:54 -0300

    chore: set up database connection and knex migrations

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
-console.log('hello world')
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/insert', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'test transactions',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app.get('/select', async () => {
+  const transaction = await knex('transactions').select('*')
+
+  return transaction
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 Started server at http://localhost:3333')
+})
